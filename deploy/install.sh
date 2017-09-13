5f1b0b8edec66eb7848a6ad42c3b59742405ca7f
Márcio de Sousa <marciodesousa16@gmail.com>
2017-09-13 23:43:47 +0200

    hopefully it will work now. fingers crossed!

--- deploy/install.sh
+++ deploy/install.sh
@@ -2,10 +2,10 @@
 set -x # Show the output of the following commands (useful for debugging)
     
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_22009518e18d_key -iv $encrypted_22009518e18d_iv -in id_digitalocean.enc -out id_digitalocean -d
-rm id_digitalocean.enc # Don't need it anymore
-chmod 600 id_digitalocean
-mv id_digitalocean ~/.ssh/id_rsa
+openssl aes-256-cbc -K $encrypted_22009518e18d_key -iv $encrypted_22009518e18d_iv -in do_deploy_key.enc -out do_deploy_key -d
+rm do_deploy_key.enc # Don't need it anymore
+chmod 600 do_deploy_key
+mv do_deploy_key ~/.ssh/id_rsa
 
 npm install -g yarn
 npm install -g webpack
