9e748dc88676e6270f1f43dc3f228b0d7a768657
Márcio de Sousa <marciodesousa16@gmail.com>
2017-09-14 23:26:21 +0200

    a whole bunch of changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,4 @@
-import _ from 'lodash';
+import React from 'react';
+import Greeting from './greeting';
 
-function component() {
-  var element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpacsdfsdfsdfsdMAH  asd  asd asd a sdasSk'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+React.render(<Greeting name='sdaasdasd'/>, document.body);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,42 +1,33 @@
-var debug = process.env.NODE_ENV !== "production";
+var path = require('path');
 var webpack = require('webpack');
 
+var javascriptEntryPath = path.resolve(__dirname, 'src', 'index.js');
+var htmlEntryPath = path.resolve(__dirname, 'src', 'index.html');
+var buildPath = path.resolve(__dirname, 'dist');
+
 module.exports = {
-    context: __dirname + "/src",
-    devtool: debug ? "inline-sourcemap": null,
-    entry: "./index.js",
-    module: {
-        loaders: [
-            {
-                test: /\.js?$/,
-                exclude: /(api|node_modules|bower_components=)/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ["es2015", "stage-0"],
-                    plugins: ["transform-class-properties", "transform-decorators-legacy"]
-                }
-            }
-        ]
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        inline: true,
-        hot: true,
-        open: true,
-        overlay: {
-            warnings: true,
-            errors: true,
-            watchContentBase: true
-        }
-    },
-    output: {
-        path: __dirname + "/dist/",
-        filename: "bundle.js"
-    },
-    plugins: debug ? [] : [
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false}),
-        new webpack.HotModuleReplacementPlugin()
-    ]
-};
+  entry: [
+    'webpack-hot-middleware/client?reload=true', 
+    javascriptEntryPath,
+    htmlEntryPath
+  ],
+  output: {
+    path: buildPath,
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      loaders: ['react-hot-loader', 'babel-loader?presets[]=react,presets[]=es2015'],
+    }, {
+      test: /\.html$/,
+      loader: 'file-loader?name=[name].[ext]',
+    }], 
+  },
+  plugins: [ 
+    // new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+}
