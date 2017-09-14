e4ce6670812db028d99a93ef47c50c5821eb3df5
Márcio de Sousa <marciodesousa16@gmail.com>
2017-09-14 21:13:33 +0200

    another shot...

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 set -x
-if [ $TRAVIS_BRANCH == 'master' ] ; then
-    git init
+# if [ $TRAVIS_BRANCH == 'master' ] ; then
+#     git init
         
-    git remote add deploy "root@146.185.133.5:/var/www/free-pooch"
-    git config user.name "Travis CI"
-    git config user.email "marciodesousa16+travisCI@gmail.com"
+#     git remote add deploy "root@146.185.133.5:/var/www/free-pooch"
+#     git config user.name "Travis CI"
+#     git config user.email "marciodesousa16+travisCI@gmail.com"
     
-    git add .
-    git commit -m "Deploy"
-    git push --force deploy master
-else
-    echo "Not deploying, since this branch isn't master."
-fi
+#     git add .
+#     git commit -m "Deploy"
+#     git push --force deploy master
+# else
+#     echo "Not deploying, since this branch isn't master."
+# fi
+
+tar -czf package.tgz . && \
+scp package.tgz root@146.185.133.5:/var/www/free-pooch
