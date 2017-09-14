f656bba038adb3afbe1bff6f670b83d5364be809
Márcio de Sousa <marciodesousa16@gmail.com>
2017-09-14 21:05:39 +0200

    another try

--- deploy/install.sh
+++ deploy/install.sh
@@ -2,7 +2,6 @@
 set -x # Show the output of the following commands (useful for debugging)
     
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_22009518e18d_key -iv $encrypted_22009518e18d_iv -in do_deploy_key.enc -out do_deploy_key -d
 rm do_deploy_key.enc # Don't need it anymore
 chmod 600 do_deploy_key
 mv do_deploy_key ~/.ssh/id_rsa
