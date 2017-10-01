653ba32946542ac4892eba45b30ba3fcb67b2cbb
Márcio de Sousa <marciodesousa16@gmail.com>
2017-10-01 13:30:26 +0200

    small test for CI

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import Greeting from './greeting';
 
-React.render(<Greeting name='sdaasdasdadasdasdaqqqqq'/>, document.body);
+React.render(<Greeting name='Marcio DE SUTTEEEERR'/>, document.body);
