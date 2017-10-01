58f8988b7f255deeaa868d04094dfd9e9d9f2cff
Márcio de Sousa <marciodesousa16@gmail.com>
2017-10-01 13:12:57 +0200

    Added docker commands for deploy.

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import Greeting from './greeting';
 
-React.render(<Greeting name='sdaasdasd'/>, document.body);
+React.render(<Greeting name='sdaasdasdadasdasdaqqqqq'/>, document.body);
--- deploy/install.sh
+++ deploy/install.sh
@@ -14,3 +14,6 @@ yarn
 
 cd ../server
 yarn
+
+cd ..
+docker-compose stop && docker-compose up
