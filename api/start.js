89e71befdf48dc221a4173de2718ef61c08e74a3
Márcio de Sousa <marciodesousa16@gmail.com>
2017-11-08 23:12:05 +0100

    now missing only a production setting... "only"

--- api/start.js
+++ api/start.js
@@ -24,7 +24,8 @@ require('./server/models/Park');
 
 // Start our app!
 const app = require('./app');
-app.set('port', process.env.PORT || 7777);
+app.set('port', process.env.PORT || 8080);
+
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
