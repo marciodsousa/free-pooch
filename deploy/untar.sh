82689dcf18843d375f0ca943d788263714ccb491
Márcio de Sousa <marciodesousa16@gmail.com>
2020-03-22 23:14:24 +0100

    Updated landing directory

--- deploy/untar.sh
+++ deploy/untar.sh
@@ -1,10 +1,10 @@
 # export NODE_ENV=production
 # export NVM_BIN=$HOME/.nvm/versions/node/v6.9.0/bin
 
-cd /var/www && \
-rm -r free-pooch
+cd ~/temp_ci && \
 tar zxf free-pooch.tgz -C . && \
 rm free-pooch.tgz
-cd free-pooch
+mv -r free-pooch /usr/share/nginx/html/free-pooch/versions/
+cd /usr/share/nginx/html/free-pooch/versions/free-pooch
 yarn prod:stop
 yarn prod:start
